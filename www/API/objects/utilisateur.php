e265326c0f277cc8a641ed28671f2edb5f4d7c8b
paulFouche <57131366+paulFouche@users.noreply.github.com>
2019-12-20 11:36:25 +0100

    utilisateur et token
    create
    update
    utilisateur

---
+++ www/API/objects/utilisateur.php
@@ -0,0 +1,150 @@
+<?php
+// 'utilisateur' object
+class Utilisateur{
+ 
+    // database connection and table name
+    private $conn;
+    private $table_name = "utilisateur";
+ 
+    // object properties
+    public $id;
+    public $prenom;
+    public $nom;
+    public $email;
+    public $password;
+    public $tel;
+ 
+    // constructor
+    public function __construct($db){
+        $this->conn = $db;
+    }
+ 
+function create(){
+ 
+    // insert query
+    $query = "INSERT INTO " . $this->table_name . "
+            SET
+                prenom = :prenom,
+                nom = :nom,
+                email = :email,
+                password = :password";
+ 
+    // prepare the query
+    $stmt = $this->conn->prepare($query);
+ 
+    // sanitize
+    $this->prenom=htmlspecialchars(strip_tags($this->prenom));
+    $this->nom=htmlspecialchars(strip_tags($this->nom));
+    $this->email=htmlspecialchars(strip_tags($this->email));
+    $this->password=htmlspecialchars(strip_tags($this->password));
+ 
+    // bind the values
+    $stmt->bindParam(':prenom', $this->prenom);
+    $stmt->bindParam(':nom', $this->nom);
+    $stmt->bindParam(':email', $this->email);
+ 
+    // hash the password before saving to database
+    $password_hash = password_hash($this->password, PASSWORD_BCRYPT);
+    $stmt->bindParam(':password', $password_hash);
+ 
+    // execute the query, also check if query was successful
+    if($stmt->execute()){
+        return true;
+    }
+ 
+    return false;
+}
+ 
+// check if given email exist in the database
+function emailExists(){
+ 
+    // query to check if email exists
+    $query = "SELECT id, prenom, nom, password
+            FROM " . $this->table_name . "
+            WHERE email = ?
+            LIMIT 0,1";
+ 
+    // prepare the query
+    $stmt = $this->conn->prepare( $query );
+ 
+    // sanitize
+    $this->email=htmlspecialchars(strip_tags($this->email));
+ 
+    // bind given email value
+    $stmt->bindParam(1, $this->email);
+ 
+    // execute the query
+    $stmt->execute();
+ 
+    // get number of rows
+    $num = $stmt->rowCount();
+ 
+    // if email exists, assign values to object properties for easy access and use for php sessions
+    if($num>0){
+ 
+        // get record details / values
+        $row = $stmt->fetch(PDO::FETCH_ASSOC);
+ 
+        // assign values to object properties
+        $this->id = $row['id'];
+        $this->prenom = $row['prenom'];
+        $this->nom = $row['nom'];
+        $this->password = $row['password'];
+        $this->email = $row['email'];
+        $this->tel = $row['tel'];
+ 
+        // return true because email exists in the database
+        return true;
+    }
+ 
+    // return false if email does not exist in the database
+    return false;
+}
+ 
+// update a user record
+public function update(){
+ 
+    // if password needs to be updated
+    $password_set=!empty($this->password) ? ", password = :password" : "";
+ 
+    // if no posted password, do not update the password
+    $query = "UPDATE " . $this->table_name . "
+            SET
+                firstname = :firstname,
+                lastname = :lastname,
+                email = :email
+                {$password_set}
+            WHERE id = :id";
+ 
+    // prepare the query
+    $stmt = $this->conn->prepare($query);
+ 
+    // sanitize
+    $this->firstname=htmlspecialchars(strip_tags($this->firstname));
+    $this->lastname=htmlspecialchars(strip_tags($this->lastname));
+    $this->email=htmlspecialchars(strip_tags($this->email));
+ 
+    // bind the values from the form
+    $stmt->bindParam(':firstname', $this->firstname);
+    $stmt->bindParam(':lastname', $this->lastname);
+    $stmt->bindParam(':email', $this->email);
+ 
+    // hash the password before saving to database
+    if(!empty($this->password)){
+        $this->password=htmlspecialchars(strip_tags($this->password));
+        $password_hash = password_hash($this->password, PASSWORD_BCRYPT);
+        $stmt->bindParam(':password', $password_hash);
+    }
+ 
+    // unique ID of record to be edited
+    $stmt->bindParam(':id', $this->id);
+ 
+    // execute the query
+    if($stmt->execute()){
+        return true;
+    }
+ 
+    return false;
+}
+
+}
---
+++ www/API/utilisateur/create.php
@@ -0,0 +1,60 @@
+<?php
+// required headers
+header("Access-Control-Allow-Origin: http://localhost/rest-api-authentication-example/");
+header("Content-Type: application/json; charset=UTF-8");
+header("Access-Control-Allow-Methods: POST");
+header("Access-Control-Max-Age: 3600");
+header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
+ 
+// database connection will be here
+
+// files needed to connect to database
+include_once 'config/database.php';
+include_once 'objects/utilisateur.php';
+ 
+// get database connection
+$database = new Database();
+$db = $database->getConnection();
+ 
+// instantiate product object
+$utilisateur = new Utilisateur($db);
+ 
+// submitted data will be here
+
+// get posted data
+$data = json_decode(file_get_contents("php://input"));
+ 
+// set product property values
+$utilisateur->prenom = $data->prenom;
+$utilisateur->nom = $data->nom;
+$utilisateur->email = $data->email;
+$utilisateur->password = $data->password;
+$utilisateur->tel = $data->tel;
+
+// create the utilisateur
+if(
+    !empty($utilisateur->prenom) &&
+    !empty($utilisateur->email) &&
+    !empty($utilisateur->password) &&
+    !empty($utilisateur->nom) &&
+    !empty($utilisateur->tel) &&
+    $utilisateur->create()
+){
+ 
+    // set response code
+    http_response_code(200);
+ 
+    // display message: utilisateur was created
+    echo json_encode(array("message" => "utilisateur was created."));
+}
+ 
+// message if unable to create utilisateur
+else{
+ 
+    // set response code
+    http_response_code(400);
+ 
+    // display message: unable to create utilisateur
+    echo json_encode(array("message" => "Unable to create utilisateur."));
+}
+?>
